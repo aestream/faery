e216478057f46d26810f9b811029674abb61761b
Andrea! <pignata.andrea@gmail.com>
2024-09-03 16:08:59 +0200

    Add UDP output support

---
+++ faery/udp.py
@@ -0,0 +1,47 @@
+from socket import socket, AF_INET, AF_INET6, SOCK_DGRAM
+
+from faery.output import EventOutput
+from faery.stream_types import Event, Events
+
+class UdpEventOutput(EventOutput):
+
+    def __init__(self, server_address:str, server_port:int, include_timestamps:bool=False, **kwargs):
+        self.clientSocket = socket(AF_INET, SOCK_DGRAM)
+        # IPv6: AF_INET6 and addr tuple becomes (host, port, flowinfo, scope_id) the former 2 optional.
+        self.clientSocket.settimeout(1)
+        self.addr = (server_address, server_port)   
+        self.include_ts = include_timestamps
+        if include_timestamps: 
+            raise NotImplementedError("Timestamps are not supported in UDP output")
+        else:
+            self.max_buffer_size = 512/4
+        self.buffer: list[bytes] = []
+        self.kwargs = kwargs
+
+    def encode_spif(self, event: dict) -> bytes:
+        # Check if the coordinates can be sent in the 32 bit packet. If not, set them to the maximum value.
+        # Should we raise an exception instead? 1920x1080 is the maximum resolution for DVS for now.
+        if (event['y'] > 2**15):
+            event['y'] = 2**15 - 1
+        if (event['x'] > 2**15):
+            event['x'] = 2**15 - 1
+        message = 0
+        message = message + int(event['y'])
+        message = message + (int(event['p']) << 15)
+        message = message + (int(event['x']) << 16)
+        message = message + (int(not self.include_ts) << 31)   # 0: present, 1: absent
+        message = message.to_bytes(length=4, byteorder='little', signed=False)
+        return message
+
+    def apply(self, data: Events):
+        for event in data:
+            message = self.encode_spif(event)
+            self.buffer.append(message)
+            if len(self.buffer) >= self.max_buffer_size:
+                self.clientSocket.sendto(b''.join(self.buffer), self.addr)
+                self.buffer.clear()
+        #If the buffer is not empty, send the remaining data
+        if len(self.buffer) > 0:
+            self.clientSocket.sendto(b''.join(self.buffer), self.addr)
+            self.buffer.clear() 
+
---
+++ tests/test_udp.py
@@ -0,0 +1,32 @@
+import faery
+from socket import socket, AF_INET, SOCK_DGRAM
+
+def decode_spif(b):
+    event_y = b[0] + ((b[1] & 0x7F) << 8) 
+    pol = bool(b[1] >> 7)
+    event_x = b[2] + ((b[3] & 0x7F) << 8) 
+    ts = not bool(b[3] >> 7)
+    return {'x': event_x, 'y': event_y, 'p': pol, 'ts': ts}
+
+
+def test_udp_event_stream():
+
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(("localhost", 12345))
+
+    faery.read_file(
+        "tests/data/sample.csv").output(
+            faery.udp.UdpEventOutput(server_address="localhost", 
+                                     server_port=12345))
+    message = serverSocket.recvfrom(1024)[0]
+    f = open("tests/data/sample.csv", "r")
+    for i in range(0, len(message), 4):
+        event_from_udp = decode_spif(message[i:i+4])
+        event_from_file = f.readline().strip().split(",")
+        assert event_from_udp['x'] == int(event_from_file[1])
+        assert event_from_udp['y'] == int(event_from_file[2])
+        assert event_from_udp['p'] == bool(int(event_from_file[3]))
+    f.close()
+    serverSocket.close()
+
+
