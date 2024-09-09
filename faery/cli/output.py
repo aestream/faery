0f0f93595be4fc7b06dbde784963521506a23757
Andrea Pignata <62184044+andreapignaz@users.noreply.github.com>
2024-09-09 18:48:58 +0200

    Add UDP output support (#9)
    * Add UDP output support with and without timestamps
    * Optimized buffer for UDP streaming using numpy
    * Delete an example file and a redundant conversion

--- faery/cli/output.py
+++ faery/cli/output.py
@@ -2,21 +2,29 @@ import click
 
 from faery.stdio import StdEventOutput
 from faery.csv import CsvEventOutput
+from faery.udp import UdpEventOutput
 
 
 @click.command(name="output")
 @click.argument("sink", type=str, default="stdout")
+@click.argument("address", type=str, default=None)
+@click.argument("port", type=int, default=None)
 @click.pass_context
-def cli_output(ctx, sink):
+def cli_output(ctx, address, port, sink):
     """
     Sends data to varying output sinks. Supported sinks include:\n
     \b
       - stdout: Standard output
+      - udp [server] [port]: UDP packets with SPIF encoding
       - *.csv: A CSV file
     """
     if sink.endswith(".csv"):
         ctx.obj.output = CsvEventOutput(sink)
     elif sink == "stdout":
         ctx.obj.output = StdEventOutput()
+    elif sink == "udp":
+        if address is None or port is None:
+            raise ValueError("UDP output requires address and port arguments")
+        ctx.obj.output = UdpEventOutput(server_address=address, server_port=port)
     else:
         raise ValueError("Unsupported output sink", sink)
