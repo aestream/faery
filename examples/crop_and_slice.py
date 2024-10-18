50ce4719633d97144ec42e4cf10db29fac97c3f2
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2024-10-18 20:02:24 +0200

    Add brackets for clarity

--- examples/crop_and_slice.py
+++ examples/crop_and_slice.py
@@ -4,17 +4,22 @@ import faery
 
 dirname = pathlib.Path(__file__).resolve().parent
 
-faery.events_stream_from_file(
-    dirname.parent / "tests" / "data" / "dvs.es",
-).crop(
-    left=110,
-    right=210,
-    top=70,
-    bottom=170,
-).time_slice(
-    start="00:00:00.400000",
-    end="00:00:00.600000",
-    zero=True,
-).to_file(
-    dirname.parent / "tests" / "data_generated" / "dvs_crop_and_slice.csv",
+(
+    faery.events_stream_from_file(
+        dirname.parent / "tests" / "data" / "dvs.es",
+    )
+    .crop(
+        left=110,
+        right=210,
+        top=70,
+        bottom=170,
+    )
+    .time_slice(
+        start="00:00:00.400000",
+        end="00:00:00.600000",
+        zero=True,
+    )
+    .to_file(
+        dirname.parent / "tests" / "data_generated" / "dvs_crop_and_slice.csv",
+    )
 )
