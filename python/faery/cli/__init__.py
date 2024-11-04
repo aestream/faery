c931ae44476fd573ec1ff4c4156677d453c5572d
jegp <jens@jepedersen.dk>
2024-11-04 15:52:48 +0100

    Used redundant import to help typing

--- python/faery/cli/__init__.py
+++ python/faery/cli/__init__.py
@@ -1,8 +1,10 @@
-from . import colormaps
-from . import commands
-from . import init
-from . import parser
-from . import render
-from . import run
+# See https://github.com/microsoft/pyright/blob/main/docs/typed-libraries.md#library-interface
+# for an explanation of the redundant import statements
+from . import colormaps as colormaps
+from . import commands as commands
+from . import init as init
+from . import parser as parser
+from . import render as render
+from . import run as run
 
 __all__ = ["colormaps", "commands", "init", "parser", "render", "run"]
